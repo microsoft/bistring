000643076026ca267620071dce785b40185ca889
Tavian Barnes <tavian.barnes@microsoft.com>
2019-07-24 10:50:55 -0400

    token: Fix alignment queries before/after the tokens

--- python/tests/test_token.py
+++ python/tests/test_token.py
@@ -6,20 +6,29 @@ import pytest
 
 
 def test_tokenization():
-    text = bistr('The quick, brown fox jumps over the lazy dog')
+    text = bistr('  The quick, brown fox jumps over the lazy dog  ')
     text = text.replace(',', '')
 
     tokens = Tokenization(text, [
-        Token.slice(text, 0, 3),
-        Token.slice(text, 4, 9),
-        Token.slice(text, 10, 15),
-        Token.slice(text, 16, 19),
-        Token.slice(text, 20, 25),
-        Token.slice(text, 26, 30),
-        Token.slice(text, 31, 34),
-        Token.slice(text, 35, 39),
-        Token.slice(text, 40, 43),
+        Token.slice(text, 2, 5),
+        Token.slice(text, 6, 11),
+        Token.slice(text, 12, 17),
+        Token.slice(text, 18, 21),
+        Token.slice(text, 22, 27),
+        Token.slice(text, 28, 32),
+        Token.slice(text, 33, 36),
+        Token.slice(text, 37, 41),
+        Token.slice(text, 42, 45),
     ])
+    assert tokens.text == text
+    assert tokens.text_bounds(1, 3) == (6, 17)
+    assert tokens.original_bounds(1, 3) == (6, 18)
+    assert tokens.bounds_for_text(0, 13) == (0, 3)
+    assert tokens.bounds_for_original(0, 13) == (0, 2)
+    assert tokens.slice_by_text(36, 47).text == bistr('lazy dog')
+    assert tokens.slice_by_original(36, 48).text == bistr('the lazy dog')
+    assert tokens.snap_text_bounds(1, 13) == (2, 17)
+    assert tokens.snap_original_bounds(36, 47) == (34, 46)
 
     tokens = tokens[1:-1]
     assert tokens.text.original == 'quick, brown fox jumps over the lazy'
